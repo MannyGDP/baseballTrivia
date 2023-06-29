f0f0fad15e029b5296126248efe82faadcea0ff1
Manny Delgado <mannygdp@gmail.com>
2023-06-28 19:45:59 -0700

    "working on JS code"

---
+++ index.js
@@ -0,0 +1,14 @@
+let playBall = [
+{
+    "base": "1st base",
+    "question": "What year were the Dodgers founded",
+    "choices": ["1873", "1920", "1883", "1905"],
+    "answer": 2
+},
+{
+    "base": "2nd base",
+    "question": "What year did the Dodgers move to Los Angeles",
+    "choices": ["1909", "1926", "1948", "1958"],
+    "answer": 3
+}
+];
